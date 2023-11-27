d6a4650c74904bdfb2266800ccc4a3879a6c1648
ckuzmick <conrad.kuzmick@icloud.com>
2023-11-26 21:42:25 -0500

    init

---
+++ src/components/box.js
@@ -0,0 +1,43 @@
+'use client'
+
+import * as THREE from 'three';
+import { useEffect, useRef } from 'react';
+
+const HomeBox = (classes) => {
+    const ref = useRef(null);
+
+    useEffect(() => {
+        var scene = new THREE.Scene();
+        var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+            camera.position.z = 2
+            camera.position.x = -1.4
+        var renderer = new THREE.WebGLRenderer();
+            renderer.setSize(window.innerWidth, window.innerHeight)
+
+        ref.current && ref.current.append(renderer.domElement);
+
+        // const geometry = new THREE.BoxGeometry(3, 3, 3);
+        const geometry = new THREE.SphereGeometry()
+        const material = new THREE.MeshNormalMaterial({ wireframe: true });
+        const cube = new THREE.Mesh(geometry, material);
+
+        scene.add(cube);
+
+        const animate = () => {
+            requestAnimationFrame(animate);
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            renderer.render(scene, camera)
+        };
+
+        animate();
+
+        // renderer.render(scene, camera);
+    });
+    
+    return(
+        <div ref={ref} className={classes}/>
+    )
+}
+
+export default HomeBox;
